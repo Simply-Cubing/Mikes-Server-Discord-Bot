f093cc1de688bf650f57f7025eb105f405130e35
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-08-09 20:39:58 +0800

    aesthetic update
    sends messages through embeds and fixes a problem where users can’t run /submit_appeal through dms due to not having a shared server with the bot

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from discord import app_commands
 from discord.ext import commands
 import os
 
+
 intents = discord.Intents().all()
-#yes i use all() don't ask
 client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
@@ -24,13 +24,12 @@ async def on_ready():
     print(f'Error syncing commands: {e}')
 
 
-#submit appeal command
+
 
 @client.tree.command(name="submit_appeal")
 @app_commands.describe(ban_reason="cause of your ban, if you choose 'other' please fill out the 'additional_info' parameter ")
 @app_commands.describe(unban_reason="why should you be unbanned")
 @app_commands.describe(ban_reason='unban reasons')
-#choices for reasons of ban
 @app_commands.choices(ban_reason=[
   discord.app_commands.Choice(name='NSFW', value = 1),
   discord.app_commands.Choice(name='Discrimination', value = 2),
@@ -47,28 +46,41 @@ async def on_ready():
 ])
 @app_commands.describe(additional_info="add extra comments OR if you chose 'other' as your ban reason put it here")
 async def add(interaction: discord.Interaction, ban_reason: discord.app_commands.Choice[int], unban_reason: str, additional_info:str=None):
-  
-  await interaction.response.send_message("Your ban appeal has been sent and is now pending.")
+
+  embed = discord.Embed(title="Appeal Sent", color = 0x992d22)
+  embed.add_field(name="Thank you for submitting", value="Your appeal is now pending, the admins will get back to you shortly")
+  await interaction.response.send_message(embed=embed)
   request = f'{interaction.user.name} was banned for {ban_reason.name}, {interaction.user.name} wants to be unbanned because "{unban_reason}", {interaction.user.name} would also like to mention that "{additional_info}", '
-    
-  user1 = client.get_user(id1)
-  user2 = client.get_user(id2)
-  await user1.send(request)
-  await user2.send(request)
-#bans user
+
+  user1 = client.get_user(Id1)
+  user2 = client.get_user(Id2)
+  embed2 = discord.Embed(title="Ban appeal submitted", color = 0xffff00)
+  embed2.add_field(name=f"Appeal submitted by {interaction.user.name}", value=request)
+  await user1.send(embed=embed2)
+  await user2.send(embed=embed2)
+
 @client.tree.command(name="ban")
 @app_commands.describe(reason = "reason for ban")
 @app_commands.describe(user="the user you want to ban")
 @commands.has_permissions(ban_members=True)
 async def ban(interaction:discord.Interaction, user:discord.Member, reason:str):
-  #bans user and dms the user that they were banned
   try:
-    await user.send(f"You have been banned from 'Mike's Server' for {reason} if you would like to submit an appeal to be unbanned please use '/submit_appeal'")
+    embed = discord.Embed(title="You have been banned", color=0)
+    embed.add_field(name = "Banned", value=f"You were banned for {reason}, if you would like to submit an appeal to be unbanned please join the server below and run '/submit_appeal'",)
+    embed2 = discord.Embed(title="HERE", color = 0x0000FF, url = "link")
+    embed2.add_field(name="Submit Appeal Server", value="please submit your ban appeal here, nowhere else.")
+    await user.send(embed=embed)
+    await user.send(embed=embed2)
+    
     await interaction.guild.ban(user,reason=reason)
-    await interaction.response.send_message(f'User {user.mention} has been banned for "{reason}"')
-  #lets the user know that they're unable to ban a user instead of returning an error
-  except:
-    await interaction.response.send_message("You do not have the permissions to ban this member")
-  
-  
-client.run(TOKEN)
+    embed3 = discord.Embed(title="User was banned", color = 0x9e578a,)
+    embed3.add_field(name = "Banned", value=f'{user.mention} has been banned for "{reason}"')
+    await interaction.response.send_message(embed = embed3)
+  except Exception as e:
+    embed4= discord.Embed(title=f"Unable to ban {user}", color = 0x992d22)
+    embed4.add_field(name="Ban failure",value=f"""An error occured when you tried to ban this user, you likely do not have the permissions to ban this member. 
+    
+    ERROR = {e}""")
+    await interaction.response.send_message(embed=embed4)
+    
+client.run(token)
