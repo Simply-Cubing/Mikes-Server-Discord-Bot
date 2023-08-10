a46c4f705c9bdb5db53ac373d170282b4ba57f39
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-08-10 18:04:13 +0800

    added a feature that makes sure that the characters in "color" are valid HEX characters
    The HEX form validity checker used to be faulty, now its fixed

--- discord-colour-converter.py
+++ discord-colour-converter.py
@@ -1,13 +1,23 @@
-#takes colour in hex
+#takes color in HEX
 color = input("Please input a colour in HEX form, with the hastag ->  ")
-#function which converts the HEX colour to discord's colour form 
+
+#valid Hex chars
+valid_char=["0","1","2","3",'4','5','6','7,','8','9','a','b','c','d','e','f','#']
+
+#function to convert to discord color
 def convert():
   lower_color = color.lower()
   discord_colour = lower_color.replace("#", "0x")
   print(discord_colour)
-#makes sure its written properly
-if len(color) == 7 and color[0] == "#":
+  
+#checks if the characters in "color" are valid Hex characters
+validation = [i in valid_char for i in color]
+valid_hex = all(validation)
+
+#makes sure the length of the color is proper hex, it begins with #, and the characters are valid characters
+if len(color) == 7 and color[0] == "#" and valid_hex==True:
   convert()
-#lets the user no that their colour is invalid
+#if the Hex color/form (idk the proper name) is invalid the program shuts off, lets the user know the HEX is invalid and tells them to run the script again
 else:
   print("not valid HEX code, please try again")
+  
