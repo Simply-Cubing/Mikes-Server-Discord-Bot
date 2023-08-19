9e337084a16ac0ed26503cbd4aeb52bb4e1ec3b6
Simply Cubing <113437319+Simply-Cubing@users.noreply.github.com>
2023-08-19 22:28:09 +0800

    Update main.py
    added a @client.event for when a user joins and other stuff i forgot i haven’t made a git commit in like a week xD

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
-from keep_alive import keep_alive
 from discord.app_commands import CommandTree
+import asyncio
 
 #client and command tree setup
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 tree = CommandTree(client)
 client.tree = tree
+
+logo_url = url
 
 #login messages
 @client.event
@@ -36,13 +38,13 @@ async def on_raw_reaction_add(payload):
     emoji_str = str(payload.emoji)
     guild = client.get_guild(payload.guild_id)
     user = guild.get_member(payload.user_id)
-    if payload.message_id == MSG_ID and emoji_str == emoji:
-        await user.add_roles(guild.get_role(bot_ping_role_id))
-    elif payload.message_id == MSG_ID and emoji_str != emoji:
+    if payload.message_id == int(os.environ['MSG_ID']) and emoji_str == emoji:
+        await user.add_roles(guild.get_role(int(os.environ['PING_ROLE_ID'])))
+    elif payload.message_id == int(os.environ['MSG_ID']) and emoji_str != emoji:
         pass
-    elif payload.message_id == MSG_ID2 and emoji_str == emoji:
-        await user.add_roles(guild.get_role(POLL_ROLE))
-    elif payload.message_id == MSG_ID2 and emoji_str != emoji:
+    elif payload.message_id == int(os.environ['MSG_ID_2']) and emoji_str == emoji:
+        await user.add_roles(guild.get_role(int(os.environ['POLL_ROLE'])))
+    elif payload.message_id == int(os.environ['MSG_ID_2']) and emoji_str != emoji:
         pass
     
 #reaction roles remove
@@ -64,6 +66,22 @@ async def on_raw_reaction_remove(payload):
         await user.remove_roles(guild.get_role(role_id2))
     elif payload.message_id == message_id and emoji_str != emoji:
         pass
+
+@client.event
+async def on_member_join(user):
+  verified_role = discord.utils.get(user.guild.roles,id = int(os.environ['VERIFIED']))
+  joined_role = discord.utils.get(user.guild.roles, id = int(os.environ['JOINED_ROLE']))
+  await user.add_roles(joined_role)
+  embed1 = discord.Embed(title = "Thank you for joining",)
+  embed1.add_field(name = 'Please wait two minutes', value='After those two minutes you will be given the "verified" role')
+  embed1.set_thumbnail(url = logo_url)
+  await user.send(embed=embed1)
+  await asyncio.sleep(30)
+  await user.add_roles(verified_role)
+  embed2 = discord.Embed(title="You have been verified. Enjoy our server!")
+  embed2.set_thumbnail(url = logo_url)
+  await user.send(embed=embed2)
+
 
 #submit appeal command
 @client.tree.command(name="submit_appeal", description = "If you were banned and would like to be unbanned use submit a ban appeal here")
@@ -89,15 +107,17 @@ async def add(interaction: discord.Interaction, ban_reason: discord.app_commands
   try:
     embed = discord.Embed(title="Appeal Sent", color = 0x992d22)
     embed.add_field(name="Thank you for submitting", value="Your appeal is now pending, the admins will get back to you shortly")
-    embed.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed.set_thumbnail(url=logo_url)
     await interaction.response.send_message(embed=embed)
     request = f'{interaction.user.name} was banned for {ban_reason.name}. {interaction.user.name} wants to be unbanned because "{unban_reason}", {interaction.user.name} would also like to mention that "{additional_info}", '
 
+    Id1 = int(os.environ['ID1'])
+    Id2 = int(os.environ['ID2'])
     user1 = client.get_user(Id1)
     user2 = client.get_user(Id2)
     embed2 = discord.Embed(title="Ban appeal submitted", color = 0xffff00)
     embed2.add_field(name=f"Appeal submitted by {interaction.user.name}", value=request)
-    embed2.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed2.set_thumbnail(url=logo_url)
     await user1.send(embed=embed2)
     await user2.send(embed=embed2)
   except Exception as e:
@@ -113,24 +133,24 @@ async def ban(interaction:discord.Interaction, user:discord.Member, reason:str):
   try:
     embed = discord.Embed(title="You have been banned", color=0)
     embed.add_field(name = "Banned", value=f"You were banned for {reason}. If you would like to submit an appeal to be unbanned please join the server below and run '/submit_appeal'",)
-    embed.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed.set_thumbnail(url=logo_url)
     embed2 = discord.Embed(title="HERE", color = 0x0000ff, url = "https://discord.gg/Jnt3A5cS")
     embed2.add_field(name="Submit Appeal Server", value="please submit your ban appeal here, nowhere else.")
-    embed2.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed2.set_thumbnail(url=logo_url)
     await user.send(embed=embed)
     await user.send(embed=embed2)
     
     await interaction.guild.ban(user,reason=reason)
     embed3 = discord.Embed(title="User was banned", color = 0x9e578a,)
     embed3.add_field(name = "Banned", value=f'{user.mention} has been banned for "{reason}"')
-    embed3.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed3.set_thumbnail(url=logo_url)
     await interaction.response.send_message(embed = embed3)
   except Exception as e:
     embed4= discord.Embed(title=f"Unable to ban {user}", color = 0x992d22)
     embed4.add_field(name="Ban failure",value=f"""An error occured when you tried to ban this user. You likely do not have the permissions to ban this member. 
     
     ERROR = {e}""")
-    embed4.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+    embed4.set_thumbnail(url=logo_url)
     await interaction.response.send_message(embed=embed4)
 
 @client.tree.command(name="warn", description = "Warns a user")
@@ -140,7 +160,7 @@ async def ban(interaction:discord.Interaction, user:discord.Member, reason:str):
 async def warn(interaction:discord.Interaction, user: discord.Member, reason:str):
   embed=discord.Embed(title="You have been warned", color = 0xff0000)
   embed.add_field(name="Warned", value=f"You were warned by {interaction.user.name} for {reason}",)
-  embed.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+  embed.set_thumbnail(url=logo_url)
   await user.send(embed=embed)
   embed2 = discord.Embed(title="User warned", color = 0x992d22)
   embed2.add_field(name=f"{user}",value="")
@@ -154,7 +174,7 @@ async def warn(interaction:discord.Interaction, user: discord.Member, reason:str
 async def deny(interaction:discord.Interaction, user: discord.Member, reason:str):
   embed=discord.Embed(title="Appeal Denied", color = 0xff0000)
   embed.add_field(name="Denied", value=f"Your ban appeal was denied for {reason}")
-  embed.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+  embed.set_thumbnail(url=logo_url)
   await user.send(embed=embed)
 
 #accept appeal command
@@ -164,16 +184,25 @@ async def deny(interaction:discord.Interaction, user: discord.Member, reason:str
 async def accept(interaction:discord.Interaction, user: discord.Member):
   embed=discord.Embed(title="Appeal Accepted", color = 0xff0000)
   embed.add_field(name="Accepted", value="Your ban appeal 'Mike's Server' was accept, you will be unbanned shortly")
-  embed.set_thumbnail(url="https://ih1.redbubble.net/image.3530640854.1334/st,small,507x507-pad,600x600,f8f8f8.u1.jpg")
+  embed.set_thumbnail(url=logo_url)
   await user.send(embed=embed)
 
 #help command
-@client.tree.command(name="help", description = "Takes you to the bot's github wiki")
+@client.tree.command(name="help", description = "Takes you to the bot's GitHub wiki")
 async def help(interaction=discord.Interaction):
   embed = discord.Embed(title="Github Wiki", color = 0xffff00, url="https://github.com/Simply-Cubing/Mikes-Server-Discord-Bot/wiki")
   embed.add_field(name="Wiki", value ="Open the Github wiki to find a command list for this bot")
+  embed.set_thumbnail(url=logo_url)
   await interaction.response.send_message(embed=embed)
 
-
-keep_alive()
+#allows moderators/admins to send embeds for annouements, etc
+@client.tree.command(name="send_embed", description = "Let's admins send annoucements and messages as embeds, for aesthetic")
+@app_commands.describe(inline = 'Select whether or not you want the embed to have the "inline" formatting')
+@commands.has_permissions(ban_members=True)
+async def send_embed(interaction: discord.Interaction, inline: bool, title: str, name:str, value:str, color:int):
+  embed= discord.Embed(title=title,color=color)
+  embed.add_field(name=name, value = value, inline = inline)
+  embed.set_thumbnail(url=logo_url)
+  await interaction.response.send_message(embed=embed)
+  
 client.run(TOKEN)
